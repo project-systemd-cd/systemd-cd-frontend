357905edd84fbc45c8944963e3d7e973538edccd
tingtt <ting.taku@gmail.com>
2023-02-24 01:39:18 +0900

    feat(pipeline): add systemd-service status

--- src/templates/PipelineDetail.tsx
+++ src/templates/PipelineDetail.tsx
@@ -1,4 +1,5 @@
 import { Pipeline, PipelineWithEmbedJobs } from 'domain/Backend/@types'
+import { SystemdServiceStatus } from 'src/molecules/SystemdServiceStatus'
 import { PipelineJobGroupList } from 'src/organisms/PipelineJobGroupList'
 import { PipelineJobGroupListHeader } from 'src/organisms/PipelineJobGroupListHeader'
 
@@ -11,10 +12,21 @@ export const PipelineDetailPage = ({
     <div className='mb-8'>
       <PipelineJobGroupListHeader pipeline={pipeline} />
     </div>
-    {'jobs' in pipeline && (
-      <PipelineJobGroupList
-        jobGroups={Array.isArray(pipeline.jobs) ? pipeline.jobs : []}
-      />
-    )}
+    <div className='ml-4 flex flex-wrap gap-8'>
+      {(pipeline.systemd_services ?? []).map((s) => (
+        <SystemdServiceStatus
+          key={`systemd-service.${s.name}`}
+          systemd_service={s}
+        />
+      ))}
+    </div>
+    <div className='divider mb-8' />
+    <div className='px-4'>
+      {'jobs' in pipeline && (
+        <PipelineJobGroupList
+          jobGroups={Array.isArray(pipeline.jobs) ? pipeline.jobs : []}
+        />
+      )}
+    </div>
   </div>
 )
